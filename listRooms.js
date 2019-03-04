a6c1b46d777605dbe2b60fd11ef60e564d5df0c2
Allen Lantz <allen.m.lantz@gmail.com>
2019-03-03 22:34:11 -0600

    List rooms tool

--- listRooms.js
+++ listRooms.js
@@ -6,5 +6,5 @@ const dir = fs.readdirSync("rooms");
 for(let roomFile of dir) {
   const data = JSON.parse(fs.readFileSync(path.join("rooms", roomFile)));
 
-  console.log(data.name);
+  console.log(data.name, " - ", roomFile);
 }
